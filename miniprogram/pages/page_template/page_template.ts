1342291b5897eab6d35372d3e7aca05959a76193
kawaiiz <690902636@qq.com>
2020-10-12 18:07:00 +0800

    201012 修改页面模板

--- miniprogram/pages/page_template/page_template.ts
+++ miniprogram/pages/page_template/page_template.ts
@@ -1,13 +1,10 @@
-const computedBehavior = require('miniprogram-computed')
-const { storeBindingsBehavior } = require('mobx-miniprogram-bindings')
-import { globalDataStore, } from '../../store/globalData/globalData'
-import { GlobalDataStore } from '../../store/globalData/data'
+
 // import { gotoLogin } from '../../../public/utils/util'
 // 获取应用实例
 const app = getApp<IAppOption>()
 type InitData = {
   IMAGEURL: string,
-} & GlobalDataStore
+} 
 
 type InitProperty = {}
 
@@ -15,11 +12,10 @@ type InitMethod = {
   onLoad(options: any): void
   onShow(): void
   onReady(): void
-  onShareAppMessage(): void
+  // onShareAppMessage(): void
 }
 
 Component<InitData, InitProperty, InitMethod>({
-  behaviors: [storeBindingsBehavior, computedBehavior],
   options: {
     addGlobalClass: true,
   },
@@ -29,24 +25,11 @@ Component<InitData, InitProperty, InitMethod>({
   properties: {
 
   },
-  storeBindings: {
-    store: globalDataStore,
-    fields: {
-      login: (store: GlobalDataStore) => store.login
-    }
-  },
   /**
    * 组件的初始数据
    */
   data: {
     IMAGEURL: app.globalData.IMAGEURL,
-  },
-  computed: {
-    typeText(_data: InitData & WechatMiniprogram.Component.PropertyOptionToData<InitProperty>) {
-      // 注意： computed 函数中不能访问 this ，只有 data 对象可供访问
-      // 这个函数的返回值会被设置到 this.data.sum 字段中
-      return '123'
-    },
   },
 
   /**
@@ -62,9 +45,9 @@ Component<InitData, InitProperty, InitMethod>({
     onReady() {
       console.log('onReady')
     },
-    onShareAppMessage() {
-      return app.globalData.transmit
-    }
+    // onShareAppMessage() {
+    //   return app.globalData.transmit
+    // }
   },
 
   lifetimes: {
